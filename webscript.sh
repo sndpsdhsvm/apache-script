e6ad8cd927c647b20cba826e0aca92fd20f81d31
sndpsdhsvm <85545319+sndpsdhsvm@users.noreply.github.com>
2023-10-08 23:17:43 +0530

    Update webscript.sh
    added multi os functionality.

--- webscript.sh
+++ webscript.sh
@@ -1,77 +1,152 @@
 #! /bin/bash
 
 #Variables declared
-package="wget unzip httpd"
-svc="httpd"
 tmp="/tmp/webfiles/"
-url=$1
+rootLoc="/var/www/html/"
+url=""
 if [[ "$url" =~ ^(http.+)/([^/]+)/([^/]+)$ ]]; then
     domain="${BASH_REMATCH[1]}"
     section1="${BASH_REMATCH[2]}"
     section2="${BASH_REMATCH[3]}"
 fi
-rootLoc="/var/www/html/"
 
-#installing Dependencies
-echo "########################################"
-echo "Installing Packages"
-echo "########################################"
-sudo yum install $package -y > /dev/null
-echo
+yum --help &> /dev/null
 
-#start and enable httpd services
-echo "########################################"
-echo "Start & enable httpd service"
-echo "########################################"
-sudo systemctl start $svc
-sudo systemctl enable $svc
-echo
+if[ $? -eq 0 ]
+then 
+    #set variables for Centos
+    package="wget unzip httpd"
+    svc="httpd"
+    
+    #installing Dependencies
+    echo "########################################"
+    echo "Installing Packages"
+    echo "########################################"
+    sudo yum install $package -y > /dev/null
+    echo
+    
+    #start and enable httpd services
+    echo "########################################"
+    echo "Start & enable httpd service"
+    echo "########################################"
+    sudo systemctl start $svc
+    sudo systemctl enable $svc
+    echo
+    
+    #create temp directory
+    echo "########################################"
+    echo "Start Artifact Deployment"
+    echo "########################################"
+    mkdir -p $tmp && cd $tmp
+    echo
+    
+    #downloading source code
+    echo "########################################"
+    echo "Cloning the source code"
+    echo "########################################"
+    wget $url
+    unzip $section2
+    echo
+    
+    #copying the artifact to the var/www/html/ dir
+    echo "########################################"
+    echo "Deploying......."
+    echo "########################################"
+    sudo cp -r ${section2%.zip}/* $rootLoc
+    echo
+    
+    #restart httpd services
+    echo "########################################"
+    echo "Restarting httpd services "
+    echo "########################################"
+    sudo systemctl restart $svc
+    echo
+    
+    #removing existing cloned files
+    echo "########################################"
+    echo "Cleaning up"
+    echo "########################################"
+    sudo rm -rf $tmp 
+    echo
+    
+    echo "########################################"
+    echo "Httpd Status"
+    echo "########################################"
+    sudo systemctl status $svc
+    echo
+    
+    echo "########################################"
+    echo "List the server root directory"
+    echo "########################################"
+    ls $rootLoc
+    echo
+else
+    #set variables for Centos
+    package="wget unzip apache2"
+    svc="apache2"
+    #installing Dependencies
+    echo "########################################"
+    echo "Installing Packages"
+    echo "########################################"
+    sudo apt update
+    sudo apt install $package -y > /dev/null
+    echo
+    
+    #start and enable apache2 services
+    echo "########################################"
+    echo "Start & enable apache2 service"
+    echo "########################################"
+    sudo systemctl start $svc
+    sudo systemctl enable $svc
+    echo
+    
+    #create temp directory
+    echo "########################################"
+    echo "Start Artifact Deployment"
+    echo "########################################"
+    mkdir -p $tmp && cd $tmp
+    echo
+    
+    #downloading source code
+    echo "########################################"
+    echo "Cloning the source code"
+    echo "########################################"
+    wget $url
+    unzip $section2
+    echo
+    
+    #copying the artifact to the var/www/html/ dir
+    echo "########################################"
+    echo "Deploying......."
+    echo "########################################"
+    sudo cp -r ${section2%.zip}/* $rootLoc
+    echo
+    
+    #restart httpd services
+    echo "########################################"
+    echo "Restarting apache2 services "
+    echo "########################################"
+    sudo systemctl restart $svc
+    echo
+    
+    #removing existing cloned files
+    echo "########################################"
+    echo "Cleaning up"
+    echo "########################################"
+    sudo rm -rf $tmp 
+    echo
+    
+    echo "########################################"
+    echo "Httpd Status"
+    echo "########################################"
+    sudo systemctl status $svc
+    echo
+    
+    echo "########################################"
+    echo "List the server root directory"
+    echo "########################################"
+    ls $rootLoc
+    echo
+fi
 
-#create temp directory
-echo "########################################"
-echo "Start Artifact Deployment"
-echo "########################################"
-mkdir -p $tmp && cd $tmp
-echo
-
-#downloading source code
-echo "########################################"
-echo "Cloning the source code"
-echo "########################################"
-wget $url
-unzip $section2
-echo
-
-#copying the artifact to the var/www/html/ dir
-echo "########################################"
-echo "Deploying......."
-echo "########################################"
-sudo cp -r ${section2%.zip}/* $rootLoc
-echo
-
-#restart httpd services
-echo "########################################"
-echo "Restarting httpd services "
-echo "########################################"
-sudo systemctl restart $svc
-echo
-
-#removing existing cloned files
-echo "########################################"
-echo "Cleaning up"
-echo "########################################"
-sudo rm -rf $tmp 
-echo
-
-echo "########################################"
-echo "Httpd Status"
-echo "########################################"
-sudo systemctl status $svc
-echo
-
-echo "########################################"
-echo "List the server root directory"
-echo "########################################"
-ls $rootLoc
-echo
-
+    
